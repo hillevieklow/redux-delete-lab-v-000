1059142992f9ff869ad193e22313ef2e9681bfbb
Hillevi Eklöw <hillevieklow@gmail.com>
2019-03-22 16:58:29 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -11,7 +11,9 @@ export default function manageBand(state = {
 
       return { ...state, bands: [...state.bands, band] }
 
+
       case 'DELETE_BAND':
+
           return {bands: state.bands.filter(band => band.id !== action.payload)}
 
     default:
